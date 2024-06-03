ebd35f81dbd79fb133db93c04c960b5c298182cb
artemplohenko@gmail.com <artemplohenko@gmail.com>
2024-06-03 19:57:36 +0300

    added new scripts

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -1,13 +1,10 @@
-const forms = () => {
-  const form = document.querySelectorAll("form"),
-    input = document.querySelectorAll("input"),
-    phoneInputs = document.querySelectorAll('input[name="user_phone"]');
+import checkNumInputs from "./checkNumInputs";
 
-  phoneInputs.forEach((item) => {
-    item.addEventListener("input", () => {
-      item.value = item.value.replace(/\D/, "");
-    });
-  });
+const forms = (state) => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+
+  checkNumInputs('input[name="user_phone"]');
 
   const message = {
     loading: "Loading...",
@@ -26,7 +23,7 @@ const forms = () => {
   };
 
   const clearInputs = () => {
-    input.forEach((item) => {
+    inputs.forEach((item) => {
       item.value = "";
     });
   };
@@ -40,6 +37,11 @@ const forms = () => {
       item.appendChild(statusMessage);
 
       const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
 
       postData("assets/server.php", formData)
         .then((res) => {
